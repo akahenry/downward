87f1b6de45e0e072170f0d8eb6cd1f31e75f843f
akahenry <henrybkavila@gmail.com>
2023-12-22 23:19:30 -0300

    feat: add tie breaking for selecting best operator in Python implementation

--- experiments/heuristics/improved_local_search_heuristic.py
+++ experiments/heuristics/improved_local_search_heuristic.py
@@ -1,11 +1,30 @@
-from typing import Tuple
+from typing import Dict, Callable, Iterable
 
 from post_hoc_heuristic import PostHocHeuristic
 
 from math import ceil
+from enum import Enum
+
+
+class TieBreakingOperation(Enum):
+    MAX_CONSTRAINT = 1
+    SUM_CONSTRAINTS = 2
+    SUM_SQUARE_CONSTRAINTS = 3
 
 
 class ImprovedLocalSearchHeuristic(PostHocHeuristic):
+
+    tie_breaking_operation: TieBreakingOperation
+
+    def __init__(
+        self,
+        operators_costs: Dict[str, int],
+        constraints_values: Dict[str, int],
+        tie_breaking_operation: TieBreakingOperation = TieBreakingOperation.SUM_SQUARE_CONSTRAINTS,
+    ):
+        super().__init__(operators_costs, constraints_values)
+        self.tie_breaking_operation = tie_breaking_operation
+
     def __compute_operator_performance(self, operator: str) -> float:
         return sum(
             [
@@ -18,7 +37,60 @@ class ImprovedLocalSearchHeuristic(PostHocHeuristic):
             ]
         )
 
-    def __get_best_operator(self) -> Tuple[str, float]:
+    def __compute_tie_breaking(self, a: str, b: str) -> str:
+        if self.tie_breaking_operation == TieBreakingOperation.MAX_CONSTRAINT:
+            return self.__compute_tie_breaking_max_constraint(a, b)
+        elif self.tie_breaking_operation == TieBreakingOperation.SUM_CONSTRAINTS:
+            return self.__compute_tie_breaking_sum_constraints(a, b)
+        elif self.tie_breaking_operation == TieBreakingOperation.SUM_SQUARE_CONSTRAINTS:
+            return self.__compute_tie_breaking_sum_square_constraints(a, b)
+
+        raise Exception("TieBreakingOperation not found")
+
+    def __compute_tie_breaking_max_constraint(self, a: str, b: str) -> str:
+        a_max_constraint_value = self.__get_max_constraint_value_for_operator(a)
+        b_max_constraint_value = self.__get_max_constraint_value_for_operator(b)
+
+        return b if a_max_constraint_value < b_max_constraint_value else a
+
+    def __compute_tie_breaking_sum_constraints(self, a: str, b: str) -> str:
+        a_sum_constraints_values = self.__get_sum_constraint_value_for_operator(a)
+        b_sum_constraints_values = self.__get_sum_constraint_value_for_operator(b)
+
+        return b if a_sum_constraints_values < b_sum_constraints_values else a
+
+    def __compute_tie_breaking_sum_square_constraints(self, a: str, b: str) -> str:
+        a_sum_constraints_values = self.__get_sum_square_constraint_value_for_operator(
+            a
+        )
+        b_sum_constraints_values = self.__get_sum_square_constraint_value_for_operator(
+            b
+        )
+
+        return b if a_sum_constraints_values < b_sum_constraints_values else a
+
+    def __get_max_constraint_value_for_operator(self, a: str) -> int:
+        return self.__get_lambda_constraint_value_for_operator(a, max)
+
+    def __get_sum_constraint_value_for_operator(self, a: str) -> int:
+        return self.__get_lambda_constraint_value_for_operator(a, sum)
+
+    def __get_sum_square_constraint_value_for_operator(self, a: str) -> int:
+        return self.__get_lambda_constraint_value_for_operator(
+            a, lambda it: sum(x**2 for x in it)
+        )
+
+    def __get_lambda_constraint_value_for_operator(
+        self, a: str, aggregator: Callable[[Iterable], int]
+    ) -> int:
+        max_constraint_value = aggregator(
+            self.constraints_values[constraint]
+            for constraint in self.operators_constraints[a]
+        )
+
+        return max_constraint_value
+
+    def __get_best_operator(self) -> str:
         best_operator: str = None
         best_operator_performance: int = None
 
@@ -34,6 +106,9 @@ class ImprovedLocalSearchHeuristic(PostHocHeuristic):
                     operator_performance,
                 )
                 continue
+
+            if best_operator_performance == operator_performance:
+                best_operator = self.__compute_tie_breaking(best_operator, operator)
 
         return best_operator
 
